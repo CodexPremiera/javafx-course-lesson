bf756bc69b1edaeda457e4a7c902c9ee7dabd1ed
Comandao <comandaoashleyken@gmail.com>
2024-01-08 20:53:26 +0800

    first commit

---
+++ Lessons/module-info/module-info.java
@@ -0,0 +1,9 @@
+module lesson01stages {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+
+    //opens example.learn_javafx to javafx.fxml;
+    exports lesson01stages;
+    exports lesson02scenes;
+}
