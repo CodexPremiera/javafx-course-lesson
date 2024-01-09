134671884fe7d413eed3bd98bae644aab6696789
Comandao <comandaoashleyken@gmail.com>
2024-01-09 21:58:11 +0800

    setup Lesson 05 - Stylesheet

---
+++ src/lessons/Lesson05Stylesheets/Stylesheet.java
@@ -0,0 +1,16 @@
+package Lesson05Stylesheets;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Stylesheet extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+}
