1eb89f926d6b194b250ccf809acc29b70a7f5d12
Comandao <comandaoashleyken@gmail.com>
2024-01-10 14:02:24 +0800

    complete Lesson 06 - Scene Switching

--- src/lessons/Lesson06SceneSwitching/SceneSwitchApplication.java
+++ src/lessons/Lesson06SceneSwitching/SceneSwitchApplication.java
@@ -1,6 +1,9 @@
 package Lesson06SceneSwitching;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class SceneSwitchApplication extends Application {
@@ -10,7 +13,14 @@ public class SceneSwitchApplication extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("scene-01.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
--- src/lessons/Lesson06SceneSwitching/SceneSwitchController.java
+++ src/lessons/Lesson06SceneSwitching/SceneSwitchController.java
@@ -1,4 +1,34 @@
 package Lesson06SceneSwitching;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SceneSwitchController {
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("scene-01.fxml"));
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("scene-02.fxml"));
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
